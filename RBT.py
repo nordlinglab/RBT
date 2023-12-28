f529f891bb1a583c5bfe2a278d5c51a263baac0b
Ray Chen <38467845+raiya5316@users.noreply.github.com>
2023-12-28 14:17:31 +0800

    Update RBT.py documentary lines
    According to https://peps.python.org/pep-0257/

--- RBT.py
+++ RBT.py
@@ -1,8 +1,18 @@
-# import packages
 import numpy as np
 
 def rbt(img_in):
+    """
+    Apply Rank-Biased Transformation (RBT) to the input image.
+
+    Parameters:
+    - img_in (numpy.ndarray): Input image to be transformed.
+
+    Returns:
+    - numpy.ndarray: Transformed image after Rank-Biased Transformation.
+    """
     img_class = img_in.dtype
+
+    # Determine format factor based on input image data type
     if img_class == np.uint8:
         format_factor = 2**8 - 1
     elif img_class == np.uint16:
@@ -12,8 +22,14 @@ def rbt(img_in):
     elif img_class == np.float64:
         format_factor = 1
 
+    # Compute unique values and ranks for the input image
     unique_vals, rank = np.unique(img_in, return_inverse=True)
     u = len(unique_vals)
-    rbt = format_factor * (rank-1) / (u-1)
+
+    # Perform Rank-Biased Transformation
+    rbt = format_factor * (rank - 1) / (u - 1)
+
+    # Convert the result to the original data type of the input image
     img_rbt = rbt.astype(img_class)
+
     return img_rbt
